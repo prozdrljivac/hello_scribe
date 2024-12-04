9e5ecd66fda71492e73e70d0df478854432ef495
Petar Cevriz <pcevriz95@gmail.com>
2024-12-04 12:28:09 +0100

    Add simple logic for starting a server

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	const serverPort string = "8000"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", requestHandler)
+
+	server := &http.Server{
+		Addr:           ":" + serverPort,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	fmt.Println("Server is running on port: ", serverPort)
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("This is the route of our API!")
+}
+
+// Learn why are these properties important
+// ReadTimeout:
+// WriteTimeout:
+// MaxHeaderBytes:
+
+// Create an API Point
+// Create a docker image
